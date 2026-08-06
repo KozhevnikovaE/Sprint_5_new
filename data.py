3b2b2a7b6119d51770561040946f0a0e33f17b29
Evgeniya Kozhevnikova <mosikhinaS@yandex.ru>
2026-08-06 14:03:39 +0400

    add data.py, helpers.py, locators.py

---
+++ data.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://stellarburgers.education-services.ru"
+LOGIN_PAGE_URL = f"{BASE_URL}/login"
+
+EXISTING_USER = {
+    "name": "Евгения",
+    "email": "jane101@yandex.ru",
+    "password": "101899"
+}
